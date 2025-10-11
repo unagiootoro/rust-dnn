dc09559fb1863cda14b73fa393889f8c96e33e0c
unagiootoro <ootoro838861@outlook.jp>
2025-10-11 15:33:38 +0900

    update

--- rust-dnn-core/tests/backprop_tests.rs
+++ rust-dnn-core/tests/backprop_tests.rs
@@ -1,12 +1,10 @@
 mod test_utils;
-use rust_dnn_core::{device::Device, error::Result, ten, tensor};
+use rust_dnn_core::{backend::Backend, device::Device, error::Result, ten, tensor::{self, Tensor}};
 
 use crate::test_utils::assert_tensor;
 
-#[test]
-fn test_backprop() -> Result<()> {
-    let device = Device::get_cpu_device();
-    let x = tensor::Tensor::from_scalar(2.0, device).requires_grad();
+fn test_backprop<B: Backend>(device: Device<B>) -> Result<()> {
+    let x = Tensor::from_scalar(2.0, device).requires_grad();
     let a = x.pow_scalar(2.0)?;
     let y = (a.pow_scalar(2.0)? + a.pow_scalar(2.0)?)?;
     assert_tensor(&y, &ten![32.0]);
@@ -15,3 +13,5 @@ fn test_backprop() -> Result<()> {
     assert_tensor(gx, &ten![64.0]);
     Ok(())
 }
+
+define_test!(test_backprop, test_backprop_cpu, test_backprop_cuda);
--- rust-dnn-core/tests/dtype_tests.rs
+++ rust-dnn-core/tests/dtype_tests.rs
@@ -1,17 +1,56 @@
 mod test_utils;
-use rust_dnn_core::{device::Device, error::Result, tensor::Tensor};
+use rust_dnn_core::{
+    backend::Backend,
+    device::Device,
+    error::Result,
+    ten,
+    tensor::{self},
+};
 
 use crate::test_utils::assert_tensor;
 
-#[test]
-fn test_add_u32() -> Result<()> {
-    let device = Device::get_cpu_device();
-    let x1 = Tensor::from_vec(vec![1, 2, 3, 4, 5, 6], vec![2, 3], device)?;
-    let x2 = Tensor::from_vec(vec![7, 8, 9, 10, 11, 12], vec![2, 3], device)?;
+fn test_add_u32<B: Backend>(device: Device<B>) -> Result<()> {
+    let x1 = ten![[0, 1, 2], [2, 3, 4]].to_device(device)?;
+    let x2 = ten![[5, 6, 7], [8, 9, 10]].to_device(device)?;
     let y = (x1 + x2)?;
-    assert_tensor(
-        &y,
-        &Tensor::from_vec(vec![8, 10, 12, 14, 16, 18], vec![2, 3], device)?,
-    );
+    assert_tensor(&y, &ten![[5, 7, 9], [10, 12, 14]]);
     Ok(())
 }
+
+define_test!(test_add_u32, test_add_u32_cpu, test_add_u32_cuda);
+
+fn test_add_f32<B: Backend>(device: Device<B>) -> Result<()> {
+    let x1 = ten![[-1.0, 0.0, 1.0], [2.0, 3.0, 4.0]].to_device(device)?;
+    let x2 = ten![[5.0, 6.0, 7.0], [8.0, 9.0, 10.0]].to_device(device)?;
+    let y = (x1 + x2)?;
+    assert_tensor(&y, &ten![[4.0, 6.0, 8.0], [10.0, 12.0, 14.0]]);
+    Ok(())
+}
+
+define_test!(test_add_f32, test_add_f32_cpu, test_add_f32_cuda);
+
+fn test_add_backward_f32<B: Backend>(device: Device<B>) -> Result<()> {
+    let x1 = ten![[-1.0, 0.0, 1.0], [2.0, 3.0, 4.0]]
+        .to_device(device)?
+        .requires_grad();
+    let x2 = ten![[5.0, 6.0, 7.0], [8.0, 9.0, 10.0]]
+        .to_device(device)?
+        .requires_grad();
+    let x3 = ten![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
+        .to_device(device)?
+        .requires_grad();
+    let y = ((&x1 + &x2) * x3)?;
+    assert_tensor(&y, &ten![[4.0, 12.0, 24.0], [40.0, 60.0, 84.0]]);
+    let grads = y.backward()?;
+    let gx1 = grads.get(&x1).unwrap();
+    assert_tensor(gx1, &ten![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]);
+    let gx2 = grads.get(&x2).unwrap();
+    assert_tensor(gx2, &ten![[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]);
+    Ok(())
+}
+
+define_test!(
+    test_add_backward_f32,
+    test_add_backward_f32_cpu,
+    test_add_backward_f32_cuda
+);
